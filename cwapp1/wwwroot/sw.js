de172e4383f33a479172334f9d47706453ae9106
vigneswaran <vigneswaran_Sugumaar@dell.com>
2018-02-19 23:44:46 +0530

    my first offline site

---
+++ cwapp1/wwwroot/sw.js
@@ -0,0 +1,20 @@
+﻿self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('v1').then(function (cache) {
+            return cache.addAll([
+                '/home',
+                '/lib/bootstrap/dist/css/bootstrap.css',
+                '/css/site.css',
+                '/lib/jquery/dist/jquery.js',
+                '/lib/bootstrap/dist/js/bootstrap.js',
+                '/js/site.js',
+            ]);
+        })
+    );
+});
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || fetch(event.request);
+        }))
+});
